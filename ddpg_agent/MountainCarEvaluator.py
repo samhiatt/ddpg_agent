a0199998378bf41ce8dc448f78628e6a75b0aa23
Sam Hiatt <samhiatt@gmail.com>
2019-07-04 13:07:13 -0700

    Adding hyperparams to MountainCarEvaluator.py

--- ddpg_agent/MountainCarEvaluator.py
+++ ddpg_agent/MountainCarEvaluator.py
@@ -4,9 +4,13 @@ import gym
 import numpy as np
 
 def evaluate_hyperparams(params):
+    
+    print("Evaluating MountainCarContinuous with params:",params)
+    eps, ou_theta = params
+    
     env = gym.make('MountainCarContinuous-v0')
 
-    agent = DDPG(env, train_during_episode=True, ou_mu=0, ou_theta=.05, ou_sigma=.25, 
+    agent = DDPG(env, train_during_episode=True, ou_mu=0, ou_theta=ou_theta, ou_sigma=.25, 
                  discount_factor=.999, replay_buffer_size=10000, replay_batch_size=1024,
                  tau_actor=.3, tau_critic=.1, 
                  relu_alpha_actor=.01, relu_alpha_critic=.01,
@@ -15,7 +19,7 @@ def evaluate_hyperparams(params):
                  bn_momentum_actor=0, bn_momentum_critic=.7,
                  hidden_layer_sizes_actor=[16,32,16], hidden_layer_sizes_critic=[[16,32],[16,32]], )
 
-    agent.train_n_episodes(30, eps=params.eps, eps_decay=1/50, action_repeat=5, run_tests=True, )
+    agent.train_n_episodes(30, eps=eps, eps_decay=1/50, action_repeat=5, run_tests=True, )
     
     # Return a negative score (since hyperopt will minimize this function)
     return -agent.history.max_test_score
