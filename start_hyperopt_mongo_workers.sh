8e0bf3756c4f82e6d3ee350adabaf12df089f42f
Sam Hiatt <samhiatt@gmail.com>
2019-07-17 11:39:29 -0700

    Make num workers per instance configurable

--- start_hyperopt_mongo_workers.sh
+++ start_hyperopt_mongo_workers.sh
@@ -2,9 +2,13 @@
 
 start_threads() {
 	echo "Starting $1 hyperopt-mongo-worker threads..."
-	for i in `seq 1 $1`; do 
-		PYTHONPATH=$PYTHONPATH:./ddpg_agent hyperopt-mongo-worker --mongo localhost:27017/hyperopt --reserve-timeout=36000000 --workdir=/tmp >> hyperopt-mongo-worker-$i.log 2>&1  & 
+	for i in `seq 1 $1`; do
+		PYTHONPATH=$PYTHONPATH:./ddpg_agent hyperopt-mongo-worker --mongo localhost:27017/hyperopt --reserve-timeout=36000000 --workdir=/tmp >> hyperopt-mongo-worker-$i.log 2>&1  &
 	done
 }
 
-cd ~/ddpg_agent && git checkout adding_quadcopter && git pull && start_threads 4
+if [ -z "$1" ]; then
+i=4;
+else i=$1;
+fi
+cd ~/ddpg_agent && git checkout adding_quadcopter && git pull && start_threads $i
