b9e814a2693dc78a43ab4163b3d59c93a7fae042
Sam Hiatt <samhiatt@gmail.com>
2019-07-16 21:06:27 -0700

    Watches hyperopt_mongo_worker logs

--- get_hyperopt_connection_command.py
+++ get_hyperopt_connection_command.py
@@ -18,9 +18,13 @@ def get_ssh_connect_command(instance, check_host_key=False):
 
 def get_hyperopt_worker_command(instance):
     #remote_command = "PYTHONPATH=$PYTHONPATH:~/ddpg_agent hyperopt-mongo-worker --mongo localhost:27017/hyperopt"
-    remote_command = "cd ddpg_agent && git pull && ./kill_hyperopt_mongo_workers.sh && ./start_hyperopt_mongo_workers.sh 4 ; /bin/bash"
+    #remote_command = "cd ddpg_agent && git pull && ./kill_hyperopt_mongo_workers.sh && ./start_hyperopt_mongo_workers.sh 4 ; /bin/bash"
+    remote_command = "cd ddpg_agent && git pull && ./kill_hyperopt_mongo_workers.sh && ./start_hyperopt_mongo_workers.sh 4"
     return get_ssh_connect_command(instance)+' "%s"'%remote_command
 
 for instance in get_instances():
     if instance['actual_status']=='running':
-        print(get_hyperopt_worker_command(instance))
+        print()
+        print(get_hyperopt_worker_command(instance)+" && "+get_ssh_connect_command(instance)+ " -R 27017:localhost:27017 \"cd ddpg_agent && while [ 1 ]; do tail -n5 hyperopt-mongo-worker-1.log ; echo ; tail -n5 hyperopt-mongo-worker-2.log ; echo ; tail -n5 hyperopt-mongo-worker-3.log ; echo ; tail -n5 hyperopt-mongo-worker-4.log ; echo ; sleep 1; done \" ")
+#         print(get_ssh_connect_command(instance)+ "\"cd ddpg_agent && tail -n5 hyperopt-mongo-worker-1.log && tail -n5 hyperopt-mongo-worker-2.log && tail -n5 hyperopt-mongo-worker-3.log && tail -n5 hyperopt-mongo-worker-4.log\" ")
+#         print(get_ssh_connect_command(instance)+ 
