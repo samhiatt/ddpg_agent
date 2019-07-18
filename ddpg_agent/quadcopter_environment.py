4cbb13c3c265862ae186dc79a7a89f9597616134
Sam Hiatt <samhiatt@gmail.com>
2019-07-18 13:46:09 -0700

    Changes reward function

--- ddpg_agent/quadcopter_environment.py
+++ ddpg_agent/quadcopter_environment.py
@@ -15,9 +15,9 @@ QuadcopterState = namedtuple("QuadcopterState",[
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
     def __init__(self, init_pose=None, init_velocities=None,
-        init_angle_velocities=None, runtime=10., target_pos=None,
-        vert_dist_thresh=1, horiz_dist_thresh=1,
-        target_steps_within_goal=1,):
+                init_angle_velocities=None, runtime=10., target_pos=None,
+                vert_dist_thresh=1, horiz_dist_thresh=1,
+                target_steps_within_goal=1 ):
         """Initialize a Task object.
         Params
         ======
@@ -35,15 +35,15 @@ class Task():
         # 6 dims for position/orientation, 6 dims for velocities, 6 dims for accelerations
         self.state_size = 18
         self.observation_space = Space(
-         self.observation_space = Space(
             np.hstack(( self.sim.lower_bounds, [-math.pi]*3, [float('-inf')]*6, [float('-inf')]*6)),
             np.hstack(( self.sim.upper_bounds, [math.pi]*3, [float('inf')]*6, [float('inf')]*6)) )
 
         # self.state_size = self.action_repeat * 12
         # self.observation_space = Space(
         #     list(np.hstack(( self.sim.lower_bounds, [ -math.pi ]*3, [float('-inf')]*6 )))*self.action_repeat,
-        #     list(np.hstack(( self.sim.upper_bounds, [ -math.pi ]*3, [float('inf') ]*6 )))*self.action_repeat,
+        #     list(np.hstack(( self.sim.upper_bounds, [ math.pi ]*3, [float('inf') ]*6 )))*self.action_repeat,
         # )
+
         # self.observation_space = Space( list(list(self.sim.lower_bounds) + \
         #                                      [ -math.pi ]*3)*self.action_repeat + [float('-inf')]*6,
         #                                list(list(self.sim.upper_bounds) + \
@@ -53,24 +53,29 @@ class Task():
 
         # Goal
         self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
-        self.target_steps_within_goal = target_steps_within_goal
-        self.steps_within_goal = 0
-        self.horiz_dist_thresh = horiz_dist_thresh
-        self.vert_dist_thresh = vert_dist_thresh
+        # self.target_steps_within_goal = target_steps_within_goal
+        # self.steps_within_goal = 0
+        # self.horiz_dist_thresh = horiz_dist_thresh
+        # self.vert_dist_thresh = vert_dist_thresh
 
         # History
         self.step_history = []
 
-    def reached_goal(self):
-        horiz_distance_from_goal = np.sqrt((self.sim.pose[0]-self.target_pos[0])**2
-                                           +(self.sim.pose[1]-self.target_pos[1])**2)
-        vert_distance_from_goal = np.abs(self.sim.pose[2]-self.target_pos[2])
-        return horiz_distance_from_goal < self.horiz_dist_thresh and \
-                vert_distance_from_goal <= self.vert_dist_thresh
+    # def reached_goal(self):
+    #     horiz_distance_from_goal = np.sqrt((self.sim.pose[0]-self.target_pos[0])**2
+    #                                        +(self.sim.pose[1]-self.target_pos[1])**2)
+    #     vert_distance_from_goal = np.abs(self.sim.pose[2]-self.target_pos[2])
+    #     return horiz_distance_from_goal < self.horiz_dist_thresh and \
+    #             vert_distance_from_goal <= self.vert_dist_thresh
 
     def get_full_state(self):
         return QuadcopterState( *self.sim.pose, *self.sim.v, *self.sim.angular_v,
                                 *self.sim.linear_accel, *self.sim.angular_accels )
+
+    def get_horiz_vert_distance_from_goal(self):
+        horiz_dist = np.sqrt((self.sim.pose[0]-self.target_pos[0])**2 +(self.sim.pose[1]-self.target_pos[1])**2)
+        vert_dist = np.abs(self.target_pos[2]-self.sim.pose[2])
+        return horiz_dist, vert_dist
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
@@ -88,8 +93,7 @@ class Task():
 #             reward += 1
 
         #Reward for horizontal distance to goal
-        horiz_dist = np.sqrt((self.sim.pose[0]-self.target_pos[0])**2 +(self.sim.pose[1]-self.target_pos[1])**2)
-        vert_dist = np.abs(self.target_pos[2]-self.sim.pose[2])
+        horiz_dist, vert_dist = self.get_horiz_vert_distance_from_goal()
         if vert_dist<10 and horiz_dist<10:
             reward += 10-vert_dist
             reward += .1*(10-horiz_dist)
@@ -98,7 +102,7 @@ class Task():
         # reward -= (self.sim.angular_v[0]/30.)**2 + (self.sim.angular_v[1]/30.)**2
 
         # Punishment for crashing (altitude < 1 m)
-        if self.sim.pose[2]<=0: reward -= 100
+        # if self.sim.pose[2]<=0: reward -= 100
 #         if self.sim.pose[2]<2: reward -= 1
         # Reward for being within goal radius
 #         horiz_distance_from_goal = np.sqrt((self.sim.pose[0]-self.target_pos[0])**2
@@ -122,7 +126,7 @@ class Task():
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
         reward = 0
-        pose_all = []
+        # pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
             reward += self.get_reward()
@@ -135,10 +139,17 @@ class Task():
                                  self.sim.v, self.sim.angular_v,
                                  self.sim.linear_accel, self.sim.angular_accels ))
 
+        # If episode is over and we're not hovering within 10 of goal height,
+        # then we must have flown off into outer space. Let's count this as a crash.
+        if done:
+            horiz_dist, vert_dist = self.get_horiz_vert_distance_from_goal()
+            if np.abs(vert_dist)>10:
+                reward -= 1000
+
         # Punish and end episode for crashing
-        if self.sim.pose[2]<=0:
-#             reward -= 100
-            done = True
+        # if self.sim.pose[2]<=0:
+        #     reward -= 1000
+        #     done = True
 
 #         # Punish for excessive rotor speeds
 #         reward -= np.sum(((rotor_speeds-self.action_space.low)/(self.action_space.high-self.action_space.low))**2)
@@ -148,9 +159,6 @@ class Task():
         # if self.sim.pose[2] > self.target_pos[2]:
         #     reward += 1000
         #     done = True
-        # Scale reward.
-        # TODO: How can the agent detect need for reward scaling automatically?
-#         reward = np.log1p(reward) if reward>0 else np.log1p(-reward)
 
         return next_state, reward, done, None
 
@@ -166,7 +174,7 @@ class Task():
                              self.sim.v, self.sim.angular_v,
                              self.sim.linear_accel, self.sim.angular_accels ))
 
-        self.steps_within_goal = 0
+        # self.steps_within_goal = 0
         return state
 
 class Space():
